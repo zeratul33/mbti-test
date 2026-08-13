3341b03759b5890c44bf390d9c2b801d085de978
zeratul33 <zeratul_he@hotmail.com>
2026-08-13 22:25:14 +0800

    在项目中添加@astrojs/sitemap依赖，并在astro.config.mjs中集成sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://mbti-style.ciciad.tech',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
